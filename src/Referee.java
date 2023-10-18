e404306a22bfacd0bbfca871dfa994a687c781a7
alexanderb <alex.boylan@kinkaid.org>
2023-10-18 15:15:08 -0500

    Added the two variables to the Referee class from the UML diagram. Have not added the selection: Deck [ ] yet. Almost Finished the askUserForSets() method and started the checkForSet method which checks whether or not the set that the user inputted was viable. I also started the addCards3 method but I have to wait for the Card and Deck classes to be kind of finished to finish that method. Done for the day Oct, 18.

--- src/Referee.java
+++ src/Referee.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  * The Referee class keeps track of a Board, which in turn 
  * holds a Deck of Cards. The Referee is responsible for communicating 
@@ -10,7 +11,8 @@
  */
 public class Referee {
 	// TODO: decide which private member variables the Referee class needs and declare them here.
-	
+	boolean isThereASet = false;
+	int currentScore = 0;
 	/**
 	 * constructor
 	 */
@@ -22,6 +24,33 @@ public class Referee {
 	public void playGame()
 	{
 		// TODO: write the Referee's playGame method.
+
 		System.out.println("Playing game."); // placeholder code
+		askUserForSets();
+	}
+	public void addCards3()
+	{
+//add 3 cards to the board if the user can't find a set
+	}
+	public String askUserForSets()
+	{
+		Scanner seeASet = new Scanner(System.in);
+		System.out.println("Do you see a set? Type yes or no.");
+		String yesOrNo = seeASet.nextLine();
+		if (yesOrNo.equals("no")){
+			addCards3();
+			return null;
+		} else {
+			Scanner set = new Scanner(System.in);
+			System.out.println("Which 3 cards make a set?");
+			String checkSet = set.nextLine();
+			return checkSet;
+		}
+	}
+	public boolean checkForSet(String checkSet)
+	{
+		if (checkSet.equals(null)) {
+
+		}
 	}
 }
