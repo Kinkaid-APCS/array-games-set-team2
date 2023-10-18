2590c98a26708e4bb563a365d9e4b56daebd3f9a
ellison <ellison.anderson2008@gmail.com>
2023-10-18 15:15:31 -0500

    initial test

--- src/Card.java
+++ src/Card.java
@@ -13,31 +13,88 @@ public class Card {
 
 	/**
 	 * constructor - given four values, initialize the card.
-	 * @param whichIcon: a number 0-2, where 0 means "*"; 1 means "o"; and 2 means "•" 
+	 *
+	 * @param whichIcon: a number 0-2, where 0 means "*"; 1 means "o"; and 2 means "•"
 	 * @param groupSize: a number 1-3
 	 * @param whichColor: a number 0-2
 	 * @param whichBackground: a number 0-2
 	 */
-	 public Card(int whichIcon, int groupSize, int whichColor, int whichBackground)
-	 {
-		//--------------------
-		// TODO: insert your code here.
-		//--------------------
-	 }
-	
-	 // TODO: write accessors for all member variables. In this case,
-	 //       you shouldn't write modifiers; a Card never changes.
-	 
-	 
-	/**
-	 * toString - get a visual description of this card.
-	 * @return the string describing this card.
-	 */
-	// TODO: you write the Card's toString
-	// hint: see https://www.geeksforgeeks.org/how-to-print-colored-text-in-java-console/
-	// so the string "/u001B[31mTest/u001B[0m" Prints Test in red letters.
-	// and "/u001B[31m/u001B[42mTest2/u001B[0m" prints Test2 in red letters on a green background.
-	//  (the /u001B[0m at the end resets the color to normal for the next thing you print.)
-	
-	
+	private int whichIcon;
+	private int groupSize;
+	private int whichColor;
+	private int whichBackground;
+	public static final String ANSI_RESET = "\u001B[0m";
+
+	public int getWhichIcon() {
+		return whichIcon;
+	}
+
+	public int getGroupSize() {
+		return groupSize;
+	}
+
+	public int getWhichColor() {
+		return whichColor;
+	}
+
+	public int getWhichBackground() {
+		return whichBackground;
+	}
+	public Card(int inwhichIcon, int ingroupSize, int inwhichColor, int inwhichBackground) {
+
+		{
+			whichIcon = inwhichIcon;
+			groupSize = ingroupSize;
+			whichColor = inwhichColor;
+			whichBackground = inwhichBackground;
+		}
+
+
+
+
+		// NOTE: there are no modifiers (setters) because we
+		//       don't want to ever change a card after we make it.
+
+		public String toString ()
+		{
+			int number = groupSize;
+			String fin = "";
+			if (whichIcon == 0){
+				String shape = "x";
+			}
+			else if (whichIcon == 1) {
+				String shape = "•";
+			}
+			else if (whichIcon == 2) {
+				String shape = "o";
+			}
+			if (whichColor == 0){
+				String color = "\u001B[32m";
+			}
+			else if (whichColor == 1) {
+				String color = "\u001B[31m";
+			}
+			else if (whichColor == 2) {
+				String color = "\u001B[35m\t";
+			}
+			if (whichBackground == 0){
+				String background = "\u001B[43m";
+			}
+			else if (whichBackground == 1) {
+				String background = "\u001B[44m\n";
+			}
+			else if (whichBackground == 2) {
+				String background = "\u001B[46m";
+			}
+			return face + " of " + suit;
+		}
+		// TODO: you write the Card's toString
+		// hint: see https://www.geeksforgeeks.org/how-to-print-colored-text-in-java-console/
+		// so the string "/u001B[31mTest/u001B[0m" Prints Test in red letters.
+		// and "/u001B[31m/u001B[42mTest2/u001B[0m" prints Test2 in red letters on a green background.
+		//  (the /u001B[0m at the end resets the color to normal for the next thing you print.)
+
+
+	}
+
 }
